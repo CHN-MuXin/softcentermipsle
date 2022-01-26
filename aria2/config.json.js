0aff4786f3e564d5249af3d2783d33d06d9254e6
mu_xin <h@ph233.cn>
2022-01-26 14:51:39 +0800

    修改 base64_decode命令不存在问题

--- aria2/config.json.js
+++ aria2/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"2.9",
-"md5":"1c4eb31451e7d05e4669465d2bbc363f",
+"version":"2.9.1",
+"md5":"ff52ee1556513bee5defc203733c4831",
 "home_url":"Module_aria2.asp",
 "title":"Aria2",
 "description":"linux下载利器",
-"build_date":"2020-08-20_11:35:11"
+"build_date":"2022-01-26_14:51:21"
 }
